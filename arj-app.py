0c16fbc2c66e824170f036c0e97f66cd0300329d
ARJ2160 <67954450+ARJ2160@users.noreply.github.com>
2020-09-14 17:54:58 +0530

    Create arj-app.py

---
+++ arj-app.py
@@ -0,0 +1,41 @@
+from telegram.ext import Updater,CommandHandler
+from Adafruit_IO import Data, Client, Feed
+import os,requests
+                              #Section 1 - Sending Value
+#Commands for Turning ON 
+def ON(bot,update):
+  value =  Data(value = 1)
+  chat_id = update.message.chat_id                             
+  bot.send_message(chat_id,text = "ON")
+  bot.send_photo(chat_id,photo = 'https://www.securityroundtable.org/wp-content/uploads/2019/03/AdobeStock_261504199-300x169.jpeg')
+  feed_value(1)
+
+#Commands for Turning OFF
+def OFF(bot,update):
+  value =  Data(value = 0)
+  chat_id = update.message.chat_id                             
+  bot.send_message(chat_id,text = "OFF")
+  bot.send_photo(chat_id,photo = 'https://motionarray.imgix.net/preview-366781-SucOpqoSyhMdoEq8-large.jpg?w=1400&q=60&fit=max&auto=format')
+  feed_value(0)
+
+def feed_value(value):
+  feeds = aio.feeds()
+  value_send = aio.send_data('light-control',value) #Sends data to specific feed
+#Command to be executed when Bot starts
+
+def start(bot,update):
+  start_message = "/ON TO TURN ON LIGHT    /OFF TO TURN OFF LIGHT"
+  bot.send_message(chat_id,text = start_message)
+  
+while True:
+  Adafruit_IO_Name = os.getenv('Adafruit-IO-Name')
+  Adafruit_IO_Key = os.getenv('Adafruit-IO-Key')
+  TOKEN = os.getenv('TOKEN')
+  aio = Client( Adafruit_IO_Name,Adafruit_IO_Key)
+  u = Updater(token ='TOKEN')#Updater is a library from telegram package( Authenticates the key)
+  dp = u.dispatcher
+  dp.add_handler(CommandHandler('ON',ON))
+  dp.add_handler(CommandHandler('OFF',OFF))
+  dp.add_handler(CommandHandler('start',start))
+  u.start_polling()
+  u.idle()
