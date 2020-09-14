d3de059cb03609347b0ebf7dafc4a99bc1211247
ARJ2160 <67954450+ARJ2160@users.noreply.github.com>
2020-09-14 18:25:44 +0530

    Update arj-app.py

--- arj-app.py
+++ arj-app.py
@@ -1,5 +1,4 @@
-from telegram.ext import Updater,CommandHandler
-from Adafruit_IO import Data, Client, Feed
+
 import requests
                               #Section 1 - Sending Value
 #Commands for Turning ON 
